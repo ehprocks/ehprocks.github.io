2d903e36d645e2a3933ba3731c7d076079a3c811
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-01-20 15:58:23 -0500

    Leaderboard fix

--- src/LeaderboardTable.tsx
+++ src/LeaderboardTable.tsx
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
-import { Table, Thead, Tbody, Tr, Th, Td, Container } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Container,
+  IconButton,
+} from "@chakra-ui/react";
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 
 const BASE_URL = "https://hackthebox.com";
 
 const LeaderboardTable = ({ data }) => {
-  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
-  const [sortKey, setSortKey] = useState(null);
 
-  const handleSort = (key) => {
-    const order = sortKey === key && sortOrder === "asc" ? "desc" : "asc";
-    const sorted = [...sortedData].sort((a, b) => {
-      if (a[key] < b[key]) return order === "asc" ? -1 : 1;
-      if (a[key] > b[key]) return order === "asc" ? 1 : -1;
-      return 0;
-    });
-
-    setSortedData(sorted);
-    setSortOrder(order);
-    setSortKey(key);
+  const handleSort = (column) => {
+    if (sortBy === column) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortBy(column);
+      setSortOrder("asc");
+    }
   };
+
+  const sortedData = data.slice().sort((a, b) => {
+    if (sortBy === "root_owns") {
+      return sortOrder === "asc"
+        ? a.root_owns - b.root_owns
+        : b.root_owns - a.root_owns;
+    } else if (sortBy === "points") {
+      return sortOrder === "asc" ? a.points - b.points : b.points - a.points;
+    } else {
+      // Default sorting by username
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      return sortOrder === "asc" ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+    }
+  });
 
   return (
     <Table
@@ -29,44 +49,70 @@ const LeaderboardTable = ({ data }) => {
     >
       <Thead>
         <Tr>
-          <Th color={"yellowgreen"} onClick={() => handleSort("avatar")}>
-            Avatar
-          </Th>
+          <Th color={"yellowgreen"}>Avatar</Th>
           <Th color={"yellowgreen"} onClick={() => handleSort("name")}>
-            Username
+            Username{" "}
+            {sortBy === "name" && (
+              <IconButton
+                size="xs"
+                aria-label="Sort Order"
+                icon={sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}
+              />
+            )}
           </Th>
           <Th color={"yellowgreen"} onClick={() => handleSort("root_owns")}>
-            Root Owns
+            Root Owns{" "}
+            {sortBy === "root_owns" && (
+              <IconButton
+                size="xs"
+                aria-label="Sort Order"
+                icon={sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}
+              />
+            )}
           </Th>
           <Th color={"yellowgreen"} onClick={() => handleSort("rank_text")}>
-            Rank
+            Rank{" "}
+            {sortBy === "rank_text" && (
+              <IconButton
+                size="xs"
+                aria-label="Sort Order"
+                icon={sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}
+              />
+            )}
           </Th>
           <Th color={"yellowgreen"} onClick={() => handleSort("points")}>
-            Points
+            Points{" "}
+            {sortBy === "points" && (
+              <IconButton
+                size="xs"
+                aria-label="Sort Order"
+                icon={sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}
+              />
+            )}
           </Th>
         </Tr>
       </Thead>
       <Tbody>
         {sortedData.map((user) => (
           <Tr key={user.id}>
-          <Td>
-            <Container display={{ base: "none", sm: "flex" }}>
-              <img
-                src={BASE_URL + user.avatar}
-                alt={user.name}
-                className="avatar"
-              />
-            </Container>
-          </Td>
-          <Td>{user.name}</Td>
-          <Td>{user.root_owns}</Td>
-          <Td>
-            <Container display={{ base: "none", sm: "flex" }}>
-              {user.rank_text}
-            </Container>
-          </Td>
-          <Td>{user.points}</Td>
-        </Tr>
+            <Td>
+              <Container display={{ base: "none", sm: "flex" }}>
+                <img
+                  src={BASE_URL + user.avatar}
+                  alt={user.name}
+                  className="avatar"
+                />
+              </Container>
+            </Td>
+            <Td>{user.name}</Td>
+            <Td>{user.root_owns}</Td>
+            <Td>
+              <Container display={{ base: "none", sm: "flex" }}>
+                {user.rank_text}
+              </Container>
+            </Td>
+            <Td>{user.points}</Td>
+          </Tr>
         ))}
       </Tbody>
     </Table>
