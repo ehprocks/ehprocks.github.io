53c4071b4664dac4690f4f4235eea40c08c68716
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-01-20 15:43:52 -0500

    fixed leaderboard prior commit
    the sorting indacator solution caused table content to not appear

--- src/LeaderboardTable.tsx
+++ src/LeaderboardTable.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td, Container } from "@chakra-ui/react";
-import { FaSortUp, FaSortDown } from "react-icons/fa"; // Import arrow icons
 
 const BASE_URL = "https://hackthebox.com";
 
@@ -22,14 +21,6 @@ const LeaderboardTable = ({ data }) => {
     setSortKey(key);
   };
 
-  const renderSortIndicator = (key) => {
-    if (sortKey === key) {
-      return sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />;
-    }
-    return null;
-  };
-
-  //table return statement
   return (
     <Table
       variant="simple"
@@ -38,54 +29,44 @@ const LeaderboardTable = ({ data }) => {
     >
       <Thead>
         <Tr>
-          <Th color={"yellowgreen"}>
-            <Container onClick={() => handleSort("avatar")} display="flex" alignItems="center">
-              Avatar {renderSortIndicator("avatar")}
-            </Container>
+          <Th color={"yellowgreen"} onClick={() => handleSort("avatar")}>
+            Avatar
           </Th>
-          <Th color={"yellowgreen"}>
-            <Container onClick={() => handleSort("name")} display="flex" alignItems="center">
-              Username {renderSortIndicator("name")}
-            </Container>
+          <Th color={"yellowgreen"} onClick={() => handleSort("name")}>
+            Username
           </Th>
-          <Th color={"yellowgreen"}>
-            <Container onClick={() => handleSort("root_owns")} display="flex" alignItems="center">
-              Root Owns {renderSortIndicator("root_owns")}
-            </Container>
+          <Th color={"yellowgreen"} onClick={() => handleSort("root_owns")}>
+            Root Owns
           </Th>
-          <Th color={"yellowgreen"}>
-            <Container onClick={() => handleSort("rank_text")} display="flex" alignItems="center">
-              Rank {renderSortIndicator("rank_text")}
-            </Container>
+          <Th color={"yellowgreen"} onClick={() => handleSort("rank_text")}>
+            Rank
           </Th>
-          <Th color={"yellowgreen"}>
-            <Container onClick={() => handleSort("points")} display="flex" alignItems="center">
-              Points {renderSortIndicator("points")}
-            </Container>
+          <Th color={"yellowgreen"} onClick={() => handleSort("points")}>
+            Points
           </Th>
         </Tr>
       </Thead>
       <Tbody>
         {sortedData.map((user) => (
           <Tr key={user.id}>
-            <Td>
-              <Container display={{ base: "none", sm: "flex" }}>
-                <img
-                  src={BASE_URL + user.avatar}
-                  alt={user.name}
-                  className="avatar"
-                />
-              </Container>
-            </Td>
-            <Td>{user.name}</Td>
-            <Td>{user.root_owns}</Td>
-            <Td>
-              <Container display={{ base: "none", sm: "flex" }}>
-                {user.rank_text}
-              </Container>
-            </Td>
-            <Td>{user.points}</Td>
-          </Tr>
+          <Td>
+            <Container display={{ base: "none", sm: "flex" }}>
+              <img
+                src={BASE_URL + user.avatar}
+                alt={user.name}
+                className="avatar"
+              />
+            </Container>
+          </Td>
+          <Td>{user.name}</Td>
+          <Td>{user.root_owns}</Td>
+          <Td>
+            <Container display={{ base: "none", sm: "flex" }}>
+              {user.rank_text}
+            </Container>
+          </Td>
+          <Td>{user.points}</Td>
+        </Tr>
         ))}
       </Tbody>
     </Table>
