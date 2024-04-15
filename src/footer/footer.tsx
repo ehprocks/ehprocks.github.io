da8f655378a43273eb47d8d86a21f6718b36b672
Tommaso <61062363+tgraewe@users.noreply.github.com>
2024-04-15 18:20:15 -0400

    formatting

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import './Footer.css';
-import insta from "../assets/insta.png"
-import linkedin from "../assets/linkedin.png"
-import discord from "../assets/discord.png"
-const Footer=()=>{
-    return(
+import insta from "../assets/insta.png";
+import linkedin from "../assets/linkedin.png";
+import discord from "../assets/discord.png";
+
+const Footer = () => {
+    return (
         <div className="footer">
             <div className="sb_footer-section_padding">
                 <div className="sb_footer-links">
@@ -42,31 +43,26 @@ const Footer=()=>{
                             <p>Contact</p>
                         </a>
                     </div>
-
                     <div className="sb_footer-links-div">
                         <h4>Social Media</h4>
                         <div className="socialmedia">
                             <a href="https://www.linkedin.com/company/ethical-hackers-of-purdue"><img src={linkedin}/></a>
                             <a href="https://www.instagram.com/ethicalhackerspurdue/"><img src={insta}/></a>
-                            <a href="https://www.instagram.com/ethicalhackerspurdue/"><img src={discord}/></a>
+                            <a href="https://discord.com/invite/uDvqUVeG"><img src={discord}/></a>
                         </div>
                     </div>
-
                     <hr></hr>
-
                     <div className="sb_footer-below">
-                        < div className="sb_footer-copyright">
+                        <div className="sb_footer-copyright">
                             <p>
                                 ©2024 <a href="">Ethical Hackers of Purdue.</a> All rights Reserved.
                             </p>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
-    )
-
+    );
 }
 
 export default Footer;
