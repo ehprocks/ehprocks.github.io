facbfd5c319ed2c867089a8fc4787cddfe5c27ee
tgraewe <61062363+tgraewe@users.noreply.github.com>
2025-01-19 21:19:38 -0500

    reworked calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Dot from "../Dot";
 import "../App.scss";
 import logo from "../ehp_test3.png";
@@ -22,41 +22,31 @@ import * as bootstrap from "bootstrap";
 
 // EVENTS
 const events = [
-  { title: 'Callout', start: '2024-08-26T19:00:00'},
-  { title: 'Kali VM setup', start: '2024-08-29T19:00:00'},
-  { title: 'LoL Event', start: '2024-09-04T19:30:00'},
-  { title: 'Career night', start: '2024-09-05T19:00:00'},
-  { title: 'Kali Crash Course', start: '2024-09-09T19:00:00'},
-  { title: 'TurtleCTF Showcase', start: '2024-09-12T19:00:00'},
-  { title: 'Reconnaissance', start: '2024-09-16T19:00:00'},
-  { title: 'Crowdstrike Recap', start: '2024-09-19T19:00:00'},
-  { title: 'Uplink Walkthrough', start: '2024-09-23T19:00:00'},
-  { title: 'Password Crackin\'', start: '2024-09-26T19:00:00'},
-  { title: 'First Box: BoardLight', start: '2024-09-30T19:00:00'},
-  { title: 'Tools of the Trade', start: '2024-10-03T19:00:00'},
-  { title: 'SQLi Bootcamp', start: '2024-10-10T19:00:00'},
-  { title: 'PrivEsc Basics', start: '2024-10-14T19:00:00'},
-  { title: 'UHP', start: '2024-10-21T19:00:00'},
-  { title: 'NCL prep', start: '2024-10-24T19:00:00'},
-  { title: 'NCL recap', start: '2024-10-28T19:00:00'},
-  { title: 'Common Web Vulnerabilities', start: '2024-11-04T19:00:00'},
-  { title: 'Juice Shop', start: '2024-11-07T19:00:00'},
-  { title: 'Juice Shop II', start: '2024-11-11T19:00:00'},
-  { title: 'Wireless Pentesting', start: '2024-11-18T19:00:00'},
-  { title: 'Hardware Hacking an IoT Device', start: '2024-11-21T19:00:00'},
-  { title: 'EOY social', start: '2024-11-25T19:00:00'}
-]
-
-
-const cancelled = [
-
-] 
+  { title: 'Callout', start: '2025-01-23T19:30:00'},
+];
 
 function Calendar() {
-  const [dots, setDots] = useState<Array<typeof Dot>>([]);
+  const [dots, setDots] = useState<typeof Dot[]>([]);
+  const calendarRef = useRef<FullCalendar>(null);
   
   useEffect(() => {
     setDots(Array.from({ length: 40 }, () => Dot));
+    
+    const handleResize = () => {
+      if (calendarRef.current) {
+        const calendarApi = calendarRef.current.getApi();
+        if (window.innerWidth < 768) {
+          calendarApi.changeView('listMonth');
+        } else {
+          calendarApi.changeView('dayGridMonth');
+        }
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check
+    
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
@@ -101,31 +91,32 @@ function Calendar() {
             >
               <div>
                 <FullCalendar
+                  ref={calendarRef}
                   plugins={[dayGridPlugin, listPlugin]}
                   initialView='dayGridMonth'
                   events={events}
                   weekends={false}
                   height={"90vh"}
                   headerToolbar={{
-                    start: "prev,next today",
+                    start: "prev,next",
                     center: "title",
-                    end: "dayGridMonth,dayGridWeek,listMonth"
+                    end: "dayGridMonth,listMonth"
                   }}
                 />
               </div>
             </GridItem>
-
           </Grid>
         </Container>
         <div
           id="dot-wrapper"
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            backgroundColor: "black",
-            zIndex: "0",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            pointerEvents: "none",
+            zIndex: -1,
           }}
         >
           {dots.map((DotComponent, index) => (
@@ -133,7 +124,7 @@ function Calendar() {
           ))}
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </ChakraProvider>
   );
 }
